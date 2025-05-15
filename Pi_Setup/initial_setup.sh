1fd708cd6d45f239195dba4723278da6a947293a
sebael5 <sebastianel5@gmai.com>
2025-05-15 07:48:53 -0300

    updating Pi_Setup

--- Pi_Setup/initial_setup.sh
+++ Pi_Setup/initial_setup.sh
@@ -1,38 +1,47 @@
-sudo passwd pi
+#Change Raspberry Pi default user's password
+sudo passwd {default_pi_user}
 
+#Install vim & ntfs-3g
 sudo apt update
 
 sudo apt upgrade -y
 
 sudo apt install -y vim && sudo apt install -y ntfs-3g
 
+#Change the Pi's name
 sudo vim /etc/hostname
  
- serverpi
+ {hostname}
 
-sudo useradd -m media -s /bin/bash
+#Create a new user to user Plex
+sudo useradd -m {user_for_plex} -s /bin/bash
 
-sudo passwd media
+sudo passwd {user_for_plex}
 
+#Add new use to sudoers
 sudo visudo
-%media   ALL=(ALL:ALL) NOPASSWD:ALL
+%{user_for_plex}   ALL=(ALL:ALL) NOPASSWD:ALL
 
-sudo mkdir -p /home/media/.ssh && sudo touch /home/media/.ssh/authorized_keys
+#Add ssh key for login without user/passwd
+sudo mkdir -p /home/{user_for_plex}/.ssh && sudo touch /home/{user_for_plex}/.ssh/authorized_keys
 
-sudo vim /home/media/.ssh/authorized_keys
+sudo vim /home/{user_for_plex}/.ssh/authorized_keys
 
  copy&paste public key
 
-sudo chmod 700 /home/media/.ssh && sudo chmod 600 /home/media/.ssh/authorized_keys
+sudo chmod 700 /home/{user_for_plex}/.ssh && sudo chmod 600 /home/{user_for_plex}/.ssh/authorized_keys
 
-sudo chown -R media:media /home/media/.ssh
+sudo chown -R {user_for_plex}:{user_for_plex} /home/{user_for_plex}/.ssh
 
 exit and login with key
 
-sudo mkdir /mnt/usb1
+#Create mount point for drive containing media files
+sudo mkdir /mnt/usb1{mount_point}
 
+#Add drive to fstab for persistant mount
 sudo vim /etc/fstab
 
- UUID=4024B77124B76894 /mnt/usb1 ntfs-3g defaults,auto 0 0
+ UUID={drive_uuid} /mnt/usb1 ntfs-3g defaults,auto 0 0
 
+#mount divre
 sudo mount -a
--- Pi_Setup/pi_overclock.sh
+++ Pi_Setup/pi_overclock.sh
@@ -1,19 +1,15 @@
 vim conf
 #uncomment to overclock the arm. 700 MHz is the default.
 #arm_freq=800
-
 over_voltage=6
 arm_freq=2147
 
 #GPU Frequency
-
 gpu_freq=850
 
 #GPU Memory
-
 gpu_mem=512
 
 #CPU to Max Capacity
-
 force_turbo=1
 
